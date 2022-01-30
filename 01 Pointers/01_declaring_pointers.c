9fcf9d9329cd160a48f2458c3b0a0f85546d6c45
seddon-software <seddon_software@btinternet.com>
2022-01-30 20:18:17 +0000

    added comments to 01

---
+++ 01 Pointers/01_declaring_pointers.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/*
+ *  In C, a pointer can point at a single type of variable.  The declaration:
+ *          int* px = &x;
+ * 
+ *  declares px as of type int*, which means a pointer to int.  On the next line we assign px as &x (address of x).  Note that this
+ *  can be done in one line as with y: 
+ *              int* py = &y;
+ *
+ *  It is important that you realize we are creating a variable called px with type int* and not a variable called *px.  The * in the 
+ *  declaration says: this variable is a pointer.  Later in the example we use * again.  However this time the * is not used in a
+ *  declaration, rather it is just used in code.  This time the * has a different meaning: dereference the pointer (get the data at the 
+ *  end of the pointer).  These different meanings of * in different contexts is a constant source of confusion for newbies:
+ *      * in declaration => I am a pointer
+ *      * in code        => dereference the pointer
+ *
+ *  Finally, note that when you want to print an address, use %p as in:
+ *              printf("px = %p\n", px);
+ */
+
+int main(void)
+{
+    int x = 100;
+    int* px;
+    px = &x;
+
+    int y = 200;
+    int* py = &y;
+
+
+    printf("x = %i\n", x);
+    printf("*px = %i\n", *px);
+
+    printf("&x = %p\n", &x);
+    printf("px = %p\n", px);
+    
+}
---
+++ 01 Pointers/03_incompatable_pointers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+ *  Pointers can only point at one type of data.  In this example we try and mix types.  The point q can only point at
+ *  doubles.  However, the line:
+ *          q = p;
+ * 
+ *  copies an int* to a double*.  Surprisingly, this compiles, but with a warning.  Nevertheless, the code goes badly wrong.  It's
+ *  true that q now points to the same place as p, namely at the int x.  However, the compiler thinks q points at a double, which 
+ *  is 8 bytes in length and therefore passes 8 bytes to printf.  These 8 bytes will be the 4 bytes of x and 4 other bytes from
+ *  the memory surrounding x.  This is probably half of the 8 bytes storage of y - essentially garbage.  Not surprisingly, printf 
+ *  goes wrong when print *q.
+ */
+
+int main(void)
+{
+    printf("sizeof double = %i\n", sizeof(double));
+    printf("sizeof int = %i\n", sizeof(int));
+    int x = 100;
+    double y = 3.14159;
+    printf("&x = %p\n", &x);
+    printf("&y = %p\n", &y);
+
+    int* p = &x;
+    double* q = &y;
+    printf("*p = %i\n", *p);
+    printf("*q = %lf\n", *q);
+
+    q = p;                      // only a warning: incompatable types
+ 
+    printf("q = %p\n", q);      // the address is correct
+    printf("*q = %i\n", *q);    // but printf get the data wrong with %i
+    printf("*q = %lf\n", *q);   // likewise with %lf
+}
